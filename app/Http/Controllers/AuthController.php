8fdb2da5875643c4345b4fc20df35ebf30a34913
juan049 <juanjrodriguez049@gmail.com>
2023-01-08 21:21:41 -0600

    2023-01-09-1

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\PasswordResetMail;
 use Carbon\Carbon;
 use App\Models\User;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Validator;
 
 
@@ -30,8 +32,7 @@ class AuthController extends Controller
             ->json([
                 'ok' => true,
                 'data' => $user,
-                'access_token' => $token,
-                'token_type' => 'Bearer'
+                'access_token' => $token
             ]);
     }
 
@@ -43,8 +44,7 @@ class AuthController extends Controller
             ->json([
                 'ok' => true,
                 'data' => $user,
-                'access_token' => $token,
-                'token_type' => 'Bearer'
+                'access_token' => $token
             ]);
     }
 
@@ -69,13 +69,21 @@ class AuthController extends Controller
             // Creo un token de reset
             $user->recover_token = Str::random(32);
             $user->recover_token_expiry_date = Carbon::now()->addHours(24);
-            $user->save();
+            $result = $user->save();
 
-            //TODO enviar un email
-            $response = generical_response(
-                ok: true,
-                message: 'Email de recuperación enviado'
-            );
+            if($result){
+                Mail::to($request->email)->send( new PasswordResetMail($request->email));
+
+                $response = generical_response(
+                    ok: true,
+                    message: 'Email de recuperación enviado'
+                );
+            }else{
+                $response = generical_response(
+                    ok: false,
+                    message: 'La operación no se pudo realizar'
+                );
+            }
         }
         return response()->json($response);
          
---
+++ app/Mail/PasswordResetMail.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Adress;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class PasswordResetMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct( private $email )
+    {
+        //
+    }
+
+    /**
+     * Get the message envelope.
+     *
+     * @return \Illuminate\Mail\Mailables\Envelope
+     */
+    public function envelope()
+    {
+        return new Envelope(
+            from: env('MAIL_FROM_ADDRESS'),
+            subject: 'Reestablece tu contraseña',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     *
+     * @return \Illuminate\Mail\Mailables\Content
+     */
+    public function content()
+    {
+        return new Content(
+            view: 'emails.password-reset',
+            with: ['email' => $this->email]
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array
+     */
+    public function attachments()
+    {
+        return [];
+    }
+}
